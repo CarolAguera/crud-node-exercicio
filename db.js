92d7a8123f4c62035d67f1e3b06217b91b2d6179
Carolina Aguera <77307756+CarolAguera@users.noreply.github.com>
2021-07-15 19:39:19 -0300

    finish

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-async function connect(){
-  if(global.connection && global.connection.state !== 'disconnected')
-      return global.connection;
+async function connect() {
+  if (global.connection && global.connection.state !== 'disconnected')
+    return global.connection;
 
   const mysql = require("mysql2/promise");
   const connection = await mysql.createConnection("mysql://root:gazin@172.17.0.2:3306/projectgazin");
@@ -9,28 +9,34 @@ async function connect(){
   return connection;
 }
 
-async function selectCustomers(){
+async function selectCustomers() {
   const conn = await connect();
   const [rows] = await conn.query('SELECT * FROM usuario;');
   return rows;
 }
+async function selectSearchCustomers(search) {
+  const conn = await connect();
+  let where = "where nome like '%" + search + "%'";  
+  const [rows] = await conn.query('SELECT * FROM usuario '+ where + ';');
+  return rows;
+}
 
-async function insertCustomer(customer){
+async function insertCustomer(customer) {
   const conn = await connect();
   const sql = 'INSERT INTO usuario(nome,sexo,idade,hobby,datanascimento) VALUES (?,?,?,?,?);';
-  const values = [customer.nome,customer.sexo.toUpperCase(),customer.idade,customer.hobby,customer.datanascimento ];
+  const values = [customer.nome, customer.sexo.toUpperCase(), customer.idade, customer.hobby, customer.datanascimento];
   return await conn.query(sql, values);
 }
-async function updateCustomer(id, customer){
+async function updateCustomer(id, customer) {
   const conn = await connect();
   const sql = 'UPDATE usuario SET nome=?,sexo=?,idade=?,hobby=?,datanascimento=? WHERE id=?';
-  const values = [customer.nome, customer.sexo, customer.idade,customer.hobby,customer.datanascimento,id];
+  const values = [customer.nome, customer.sexo, customer.idade, customer.hobby, customer.datanascimento, id];
   return await conn.query(sql, values);
 }
-async function deleteCustomer(id){
+async function deleteCustomer(id) {
   const conn = await connect();
   const sql = 'DELETE FROM usuario where id=?;';
   return await conn.query(sql, [id]);
 }
 
-module.exports = {selectCustomers, insertCustomer, updateCustomer, deleteCustomer}
+module.exports = { selectCustomers, selectSearchCustomers,insertCustomer, updateCustomer, deleteCustomer }
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const { data } = require("jquery");
     res.render('cadastro-usuario')
   })
 
-  
+
   app.post('/usuario/cadastrar', async (req, res) => {
     let nome = req.body.nome
     let sexo = req.body.sexo
@@ -26,7 +26,12 @@ const { data } = require("jquery");
   })
 
   app.get('/usuario/listar', async (req, res) => {
-    const clientes = await db.selectCustomers();
+    let clientes = [];
+    if (req.query.search != undefined) {
+      clientes = await db.selectSearchCustomers(req.query.search);
+    } else {
+      clientes = await db.selectCustomers();
+    }
 
     clientes.forEach((cliente, index) => {
       let dataAtual = new Date(cliente.datanascimento);
@@ -48,25 +53,23 @@ const { data } = require("jquery");
     res.render('listar-usuario', { clientes: clientes })
   })
 
-//const result2 = await db.updateCustomer(4, { nome: "Zé José", cpfcnpj: "opa", email: "carolzinha@gmail.com", telefone: 80808080, endereco: "rua dos bobos", numero: 0, cidade: "Umuarama", estado: "PR", status: 1, cep: "0000000" });
- 
 
-    app.post('/usuario/atualizar', async (req, res) => {
-      let id = req.body.id
-      let nome = req.body.nome
-      let sexo = req.body.sexo
-      let idade = req.body.idade
-      let hobby = req.body.hobby
-      let datanascimento = req.body.datanascimento
-       await db.updateCustomer(id, { nome:nome, sexo: sexo, idade: idade, hobby: hobby, datanascimento: datanascimento });
-      res.redirect('/usuario/listar')
-    });
+  app.post('/usuario/atualizar', async (req, res) => {
+    let id = req.body.id
+    let nome = req.body.nome
+    let sexo = req.body.sexo
+    let idade = req.body.idade
+    let hobby = req.body.hobby
+    let datanascimento = req.body.datanascimento
+    await db.updateCustomer(id, { nome: nome, sexo: sexo, idade: idade, hobby: hobby, datanascimento: datanascimento });
+    res.redirect('/usuario/listar')
+  });
 
-    app.post('/usuario/excluir', async (req, res) => {
-      let id = req.body.id
-      await db.deleteCustomer(id);
-      res.redirect('/usuario/listar')
-    });
+  app.post('/usuario/excluir', async (req, res) => {
+    let id = req.body.id
+    await db.deleteCustomer(id);
+    res.redirect('/usuario/listar')
+  });
 
 
   app.listen(3000, () => { console.log('ESTÁ RODANDO NA PORTA 3000') })
